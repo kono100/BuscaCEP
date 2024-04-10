2ae53c46cff99b3ca44c65b2fa005aa40f824d6e
Thiago Campos <thiago-kono@hotmail.com>
2024-04-10 19:17:37 -0300

    add Controller e Service

---
+++ BuscaCEP/Controllers/CepController.cs
@@ -0,0 +1,27 @@
+﻿using BuscaCEP.Models;
+using BuscaCEP.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuscaCEP.Controllers
+{
+    public class CepController : Controller
+    {
+
+        public readonly CorreiosService _correiosService;
+
+        public CepController(CorreiosService correiosService)
+        {
+            _correiosService = correiosService;
+        }
+        [HttpPost]
+        public async Task<IActionResult> BuscarEnderecoPorCep(string cep)
+        {
+            Endereco? endereco = await _correiosService.BuscarEnderecoPorCep(cep);
+            if (endereco == null)
+            {
+                TempData["Mensagem"] = "CEP <strong> Não </strong> encontrado";
+            }
+            return View("Index", endereco);
+        }
+    }
+}
---
+++ BuscaCEP/Service/CorreiosService.cs
@@ -0,0 +1,6 @@
+﻿namespace BuscaCEP.Service
+{
+    public class CorreiosService
+    {
+    }
+}
